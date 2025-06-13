6d69d923173e72eb79b784b6d333d602a0f73d65
yyccbb <yuchenbo0130@gmail.com>
2025-06-13 19:18:49 +0800

    Modify model_download

--- model_download.py
+++ model_download.py
@@ -1,6 +1,7 @@
 from huggingface_hub import snapshot_download
 
-repo_id = "hugging-quants/Llama-4-Scout-17B-16E-Instruct-fbgemm-unfused"
+# repo_id = "hugging-quants/Llama-4-Scout-17B-16E-Instruct-fbgemm-unfused"
+repo_id = "meta-llama/Llama-4-Scout-17B-16E-Instruct"
 
 # Download the model to the default Hugging Face cache location
 # (usually ~/.cache/huggingface/hub/)
