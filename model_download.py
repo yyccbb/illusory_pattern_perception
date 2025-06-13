3a6eb243a02e5630144c4d277d1d886b6dbc0353
qingqing1101 <1054543012@qq.com>
2025-06-13 13:42:56 +0800

    Initial commit

---
+++ model_download.py
@@ -0,0 +1,9 @@
+from huggingface_hub import snapshot_download
+
+repo_id = "hugging-quants/Llama-4-Scout-17B-16E-Instruct-fbgemm-unfused"
+
+# Download the model to the default Hugging Face cache location
+# (usually ~/.cache/huggingface/hub/)
+downloaded_path = snapshot_download(repo_id=repo_id)
+
+print(f"Model downloaded to: {downloaded_path}")
